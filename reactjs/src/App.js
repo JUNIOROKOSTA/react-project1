1671dd490918080fa2f2b6255a666c8fa286fb5d
JUNIOR_OKOSTA <82908127+JUNIOROKOSTA@users.noreply.github.com>
2023-01-23 17:41:40 -0300

    tests useState
    testeando Hoocks no react

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,15 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//       </header>
+//     </div>
+//   );
+// }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       reverse: true,
+//     };
+//   }
+
+//   render() {
+//     const { reverse } = this.state;
+//     const mapReverse = reverse ? 'reverse' : '';
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className={`App-logo ${mapReverse}`} alt="logo" />
+//           <button type="button" onClick={() => this.setState({ reverse: !reverse })}>
+//             Spin {mapReverse}
+//           </button>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 function App() {
+  // const { reverse } = this.state;
+  const [reverse, setReverse] = useState(false);
+  const mapReverse = reverse ? 'reverse' : '';
+  const [counter, setCounter] = useState(0);
+
+  const handleClick = () => {
+    setCounter(counter + 1);
+    setReverse(!reverse);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Base de configurações Padrão Okosta para desenvolvimento em React.</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
+        <h1>Contador: {counter}</h1>
+        <img src={logo} className={`App-logo ${mapReverse}`} alt="logo" />
+        <button type="button" onClick={handleClick}>
+          Spin {mapReverse}
+        </button>
       </header>
     </div>
   );
